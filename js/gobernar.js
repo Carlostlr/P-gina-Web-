4760a3eb24c31bcd0e6a726d20eff0111504b9f9
Carlostlr <43970963+Carlostlr@users.noreply.github.com>
2018-10-10 20:02:38 +0200

    Add files via upload

---
+++ js/gobernar.js
@@ -0,0 +1,43 @@
+// boton para subir
+function arriba(sub) {sub.src="./imagenes/arriba1.png"}
+function arriba1(sub) {sub.src="./imagenes/arriba.png"}
+
+// botones del menu
+function entra1(caja){caja=document.getElementById("botones1");
+caja.style.backgroundColor="f9b50f"}
+function sale1(caja){caja=document.getElementById("botones1");
+caja.style.backgroundColor="transparent"}
+
+function entra2(caja){caja=document.getElementById("botones2");
+caja.style.backgroundColor="f9b50f"}
+function sale2(caja){caja=document.getElementById("botones2");
+caja.style.backgroundColor="transparent"}
+
+function entra3(caja){caja=document.getElementById("botones3");
+caja.style.backgroundColor="f9b50f"}
+function sale3(caja){caja=document.getElementById("botones3");
+caja.style.backgroundColor="transparent"}
+
+function entra4(caja){caja=document.getElementById("botones4");
+caja.style.backgroundColor="f9b50f"}
+function sale4(caja){caja=document.getElementById("botones4");
+caja.style.backgroundColor="transparent"}
+
+function entra5(caja){caja=document.getElementById("botones5");
+caja.style.backgroundColor="f9b50f"}
+function sale5(caja){caja=document.getElementById("botones5");
+caja.style.backgroundColor="transparent"}
+
+// fotos de twiter y eso 
+function aparece(foto) {foto.src="./imagenes/twiter1.png"}
+function desaparece(foto) {foto.src="./imagenes/twiter.png"}
+
+function aparece1(foto) {foto.src="./imagenes/google1.png"}
+function desaparece1(foto) {foto.src="./imagenes/google.png"}
+
+function aparece2(foto) {foto.src="./imagenes/facebook1.png"}
+function desaparece2(foto) {foto.src="./imagenes/facebook.png"}
+
+// imagenes
+function rota1(caja) {caja.style.transform="rotateX(270deg)"}
+function vuelve1(caja) {caja.style.transform="rotateX(0deg)"}
---
+++ js/soporte.js
@@ -0,0 +1,120 @@
+// fotos de twiter y eso 
+function aparece(foto) {foto.src="./imagenes/twiter1.png"}
+function desaparece(foto) {foto.src="./imagenes/twiter.png"}
+
+function aparece1(foto) {foto.src="./imagenes/google1.png"}
+function desaparece1(foto) {foto.src="./imagenes/google.png"}
+
+function aparece2(foto) {foto.src="./imagenes/facebook1.png"}
+function desaparece2(foto) {foto.src="./imagenes/facebook.png"}
+
+// botones del menu
+function entra1(caja){caja=document.getElementById("botones1");
+caja.style.backgroundColor="f9b50f"}
+function sale1(caja){caja=document.getElementById("botones1");
+caja.style.backgroundColor="transparent"}
+
+function entra2(caja){caja=document.getElementById("botones2");
+caja.style.backgroundColor="f9b50f"}
+function sale2(caja){caja=document.getElementById("botones2");
+caja.style.backgroundColor="transparent"}
+
+function entra3(caja){caja=document.getElementById("botones3");
+caja.style.backgroundColor="f9b50f"}
+function sale3(caja){caja=document.getElementById("botones3");
+caja.style.backgroundColor="transparent"}
+
+function entra4(caja){caja=document.getElementById("botones4");
+caja.style.backgroundColor="f9b50f"}
+function sale4(caja){caja=document.getElementById("botones4");
+caja.style.backgroundColor="transparent"}
+
+function entra5(caja){caja=document.getElementById("botones5");
+caja.style.backgroundColor="f9b50f"}
+function sale5(caja){caja=document.getElementById("botones5");
+caja.style.backgroundColor="transparent"}
+
+// linea de tiempo
+	function tiempo1(tiem) {tiem.style.width="40%";
+	var texto;
+	texto=document.getElementById("tie1");
+	texto.innerHTML="1475 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	Origen";}
+
+	function tiemp1(tiem) {tiem.style.width="8%";
+	var texto;
+	texto=document.getElementById("tie1");
+	texto.innerHTML="1475"}
+// --------------------------------------------------------------------
+	function tiempo2(tiem) {tiem.style.width="40%";
+	var texto;
+	texto=document.getElementById("tie2");
+	texto.innerHTML="1500 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	Viaje a las Indias";}
+
+	function tiemp2(tiem) {tiem.style.width="8%";
+	var texto;
+	texto=document.getElementById("tie2");
+	texto.innerHTML="1500"}
+// --------------------------------------------------------------------
+	function tiempo3(tiem) {tiem.style.width="40%";
+	var texto;
+	texto=document.getElementById("tie3");
+	texto.innerHTML="1509 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	Viaje a Tierra Firme";}
+
+	function tiemp3(tiem) {tiem.style.width="8%";
+	var texto;
+	texto=document.getElementById("tie3");
+	texto.innerHTML="1509"}
+// --------------------------------------------------------------------
+	function tiempo4(tiem) {tiem.style.width="40%";
+	var texto;
+	texto=document.getElementById("tie4");
+	texto.innerHTML="1511 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	Lucha por el poder en Veragua";}
+
+	function tiemp4(tiem) {tiem.style.width="8%";
+	var texto;
+	texto=document.getElementById("tie4");
+	texto.innerHTML="1511"}
+// --------------------------------------------------------------------
+	function tiempo5(tiem) {tiem.style.width="40%";
+	var texto;
+	texto=document.getElementById("tie5");
+	texto.innerHTML="1512 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	Gobernador de Veragua";}
+
+	function tiemp5(tiem) {tiem.style.width="8%";
+	var texto;
+	texto=document.getElementById("tie5");
+	texto.innerHTML="1512"}
+// --------------------------------------------------------------------
+	function tiempo6(tiem) {tiem.style.width="40%";
+	var texto;
+	texto=document.getElementById("tie6");
+	texto.innerHTML="1513 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	Descubrimiento y conquista del Mar del Sur";}
+
+	function tiemp6(tiem) {tiem.style.width="8%";
+	var texto;
+	texto=document.getElementById("tie6");
+	texto.innerHTML="1513"}
+// --------------------------------------------------------------------
+	function tiempo7(tiem) {tiem.style.width="40%";
+	var texto;
+	texto=document.getElementById("tie7");
+	texto.innerHTML="1514 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	Disputas con Pedrarias";}
+
+	function tiemp7(tiem) {tiem.style.width="8%";
+	var texto;
+	texto=document.getElementById("tie7");
+	texto.innerHTML="1514"}
+// --------------------------------------------------------------------
+	function tiempo8(tiem) {tiem.style.width="40%";
+	var texto;
+	texto=document.getElementById("tie8");
+	texto.innerHTML="1516-1519 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	Ocaso y muerte";}
+
+	function tiemp8(tiem) {tiem.style.width="8%";
+	var texto;
+	texto=document.getElementById("tie8");
+	texto.innerHTML="<FONT SIZE=2%>1516-1519</FONT>"}
+//---------------------------------------------------------------------
+function arriba(sub) {sub.src="./imagenes/arriba1.png"}
+
+	function arriba1(sub) {sub.src="./imagenes/arriba.png"}
